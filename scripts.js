564ccf2743e3db78307448dacc7c549fb569e281
KannKaro <111279140+KannKaro@users.noreply.github.com>
2023-10-12 15:56:49 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -14,8 +14,7 @@ let showTime = document.getElementById('time')
 let checkWord = document.getElementById('check')
 let maxTime = 30
 let oneSecond = 1000
-let randomWord = words[Math.floor(Math.random() * words.length)] //wybieram losowe slowo z listy
-// Ponizej losuje kolejnosc literek w slowie
+let randomWord = words[Math.floor(Math.random() * words.length)]
 let shuffledLetters = randomWord.split('').sort(function () { return 0.5 - Math.random() }).join('');
 console.log(randomWord)
 
@@ -26,7 +25,6 @@ window.addEventListener('load', function () {
 const timer = setInterval(function () {
     maxTime--
     showTime.innerHTML = maxTime + 's';
-    //Ponizej jezeli timer wejdzie na 0 to strona sie odswieza
     if (maxTime === 0) {
         window.location.reload()
     }
