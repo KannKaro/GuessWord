295ccda4796e904633a0c78c8f5e3bf52a4c29e7
KannKaro <111279140+KannKaro@users.noreply.github.com>
2023-10-12 17:06:48 +0200

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-let words = [ //slowa ktore sa uzywane do gry
+let words = [
     'KAWIARNIA',
     'FINANSE',
     'NIEBO',
@@ -9,29 +9,39 @@ let words = [ //slowa ktore sa uzywane do gry
     'KRAWAT',
     'CHMURA'
 ]
-let showGameWord = this.document.getElementById('word')
-let showTime = document.getElementById('time')
-let checkWord = document.getElementById('check')
+let randomWord
 let maxTime = 30
-let oneSecond = 1000
-let randomWord = words[Math.floor(Math.random() * words.length)]
-let shuffledLetters = randomWord.split('').sort(function () { return 0.5 - Math.random() }).join('');
-console.log(randomWord)
+const ONE_SECOND = 1000
+let checkWord = document.getElementById('checkWord')
 
-window.addEventListener('load', function () {
+window.addEventListener('load', getRandomAndShuffledWord())
+
+function getRandomAndShuffledWord() {
+    randomWord = getRandomWord()
+    let shuffledLetters = getShuffledLetters()
+    let showGameWord = this.document.getElementById('showGameWord')
     showGameWord.innerHTML = shuffledLetters
-})
+}
+function getRandomWord() {
+    return words[Math.floor(Math.random() * words.length)]
+}
+
+function getShuffledLetters() {
+    return randomWord.split('').sort(function () { return 0.5 - Math.random() }).join('');
+}
+
 
 const timer = setInterval(function () {
+    let showTime = document.getElementById('showTime')
     maxTime--
     showTime.innerHTML = maxTime + 's';
     if (maxTime === 0) {
         window.location.reload()
     }
-}, oneSecond)
+}, ONE_SECOND)
 
 checkWord.addEventListener('click', function () {
-    let yourInput = document.getElementById('text-container').value
+    let yourInput = document.getElementById('yourInput').value
     if (yourInput.toUpperCase() === randomWord) {
         alert('Brawo zgadles')
         window.location.reload()
